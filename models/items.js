984118d37d53cf6037c6faf2e060216ef7975977
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-20 13:08:50 -0400

    created item model and linked to server

---
+++ models/items.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const itemSchema = new mongoose.Schema ({
+    item: {type: String, required: true},
+    // user can only choose 1 out of these 4 categories
+    category: {type: String, enum: ['produce', 'meat', 'pantry/frozen', 'non-food']},
+    price: {type: Number, min: 0, required: true},
+    units: {type: number, min: 0, required: true},
+    qtyPerUnit: {type: number, min: 0, required: true}, 
+    imageURL: String, 
+    notes: String
+})
+
+const Item = mongoose.model('Item', itemSchema)
+module.exports = Item    
---
+++ server.js
@@ -0,0 +1,21 @@
+// REQUIRE ======================================================
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+require('dotenv').config()
+const PORT = process.env.PORT 
+const MONGODB_URI = process.env.MONGODB_URI
+
+const mongoose = require('mongoose')
+mongoose.connect(`${MONGODB_URI}`, {
+    useNewUrlParser: true
+})
+mongoose.connection.once('open', () => {
+    console.log('Connected to Mongo!')
+})
+
+
+// CONNECT CONTROLLER TO MODEL ==================================
+const Item = require('./models/items')
+
+// USE ==========================================================
