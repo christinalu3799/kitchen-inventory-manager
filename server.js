fc6aa6e0ec3b39d6fad2990ca4391528423e9d17
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-20 13:19:32 -0400

    created itemsController.js and connected routes to server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config()
 const PORT = process.env.PORT 
 const MONGODB_URI = process.env.MONGODB_URI
 
+// MIDDLEWARE ===================================================
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+// CONNECT TO MONGOOSE SERVER ===================================
 const mongoose = require('mongoose')
 mongoose.connect(`${MONGODB_URI}`, {
     useNewUrlParser: true
@@ -18,9 +25,9 @@ mongoose.connection.once('open', () => {
 // CONNECT CONTROLLER TO MODEL ==================================
 const Item = require('./models/items')
 
-
-
-
+ // IMPORT ROUTER ===============================================
+const itemsController = require('./controllers/itemsController')
+app.use('/inventory', itemsController)
 
 // LISTENER =====================================================
 app.listen(PORT, () => {
