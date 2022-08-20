ac1487cbace939969a547c4bb865d2085c058931
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-20 14:56:54 -0400

    created index page that displays 4 categories of items

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,14 +1,21 @@
+const { application } = require('express')
 const express = require('express')
 const router = express.Router()
 const Item = require('../models/items')
-
+const categories = ['produce', 'meat', 'pantryOrFrozen', 'non-food']
 // INDEX ROUTE =======================================================
 router.get('/', (req, res) => {
     Item.find({}, (err, allItems) => {
+        console.log(allItems)
         res.render('index.ejs', {
-            items: allItems
+            items: allItems,
+            categories: categories  
         })
     })
+})
+// Categories --------------------------------------------------------
+router.get('/:category', (req, res) => {
+    res.send(`On ${req.params.category} page.`)
 })
 // SHOW ROUTE ========================================================
 
--- models/items.js
+++ models/items.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 const itemSchema = new mongoose.Schema ({
     item: {type: String, required: true},
     // user can only choose 1 out of these 4 categories
-    category: {type: String, enum: ['produce', 'meat', 'pantry/frozen', 'non-food']},
+    category: {type: String, enum: ['produce', 'meat', 'pantryOrFrozen', 'non-food']},
     price: {type: Number, min: 0, required: true},
     units: {type: Number, min: 0, required: true},
-    qtyPerUnit: {type: Number, min: 0, required: true}, 
+    qtyPerUnit: {type: String, required: true}, 
     imageURL: String, 
     notes: String
-})
+}, {timestamps: true})
 
 const Item = mongoose.model('Item', itemSchema)
 module.exports = Item    
