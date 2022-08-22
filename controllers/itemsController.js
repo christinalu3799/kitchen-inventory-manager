4255f78b3ecba6ef5b29a1fb0e18b7aa5ec8a785
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-22 15:49:53 -0400

    added imgs to index page and added styling

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -2,7 +2,25 @@ const { application } = require('express')
 const express = require('express')
 const router = express.Router()
 const Item = require('../models/items')
-const categories = ['produce', 'meat', 'pantryOrFrozen', 'nonfood']
+const categories = [
+    {
+        type:'produce',
+        imageURL: 'https://imageio.forbes.com/specials-images/imageserve//62ba41b89c8d05e43e7a1f0d/0x0.jpg?format=jpg&width=1200'
+    },
+    {
+        type:'meat',
+        imageURL: 'https://images.ctfassets.net/3s5io6mnxfqz/5GlOYuzg0nApcehTPlbJMy/140abddf0f3f93fa16568f4d035cd5e6/AdobeStock_175165460.jpeg'
+    },
+    {
+        type:'pantryOrFrozen',
+        imageURL: 'https://www.thedollarstretcher.com/wp-content/uploads/2020/02/what-to-know-about-grocery-store-aisles.jpg'
+    },
+    {
+        type:'nonfood',
+        imageURL: 'https://cdnimg.webstaurantstore.com/uploads/seo_category/2020/6/cleaning_toolsv2.jpg'
+    }
+
+]
 const categoriesTitles = ['Produce', 'Meat', 'Pantry/Frozen', 'Non-Food']
 
 // INDEX ROUTE =======================================================
