177233b41376806281229b10aef9aa4f1e68bf7c
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-22 22:17:04 -0400

    styled show page for each item

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -3,33 +3,34 @@ const express = require('express')
 const router = express.Router()
 const Item = require('../models/items')
 const categories = [
-    {
-        type:'produce',
+    {   
+        category:'produce',
+        title: 'Produce',
         imageURL: 'https://imageio.forbes.com/specials-images/imageserve//62ba41b89c8d05e43e7a1f0d/0x0.jpg?format=jpg&width=1200'
     },
     {
-        type:'meat',
+        category:'meat',
+        title: 'Meat',
         imageURL: 'https://images.ctfassets.net/3s5io6mnxfqz/5GlOYuzg0nApcehTPlbJMy/140abddf0f3f93fa16568f4d035cd5e6/AdobeStock_175165460.jpeg'
     },
     {
-        type:'pantryOrFrozen',
+        category:'pantryOrFrozen',
+        title: 'Pantry/Frozen',
         imageURL: 'https://www.thedollarstretcher.com/wp-content/uploads/2020/02/what-to-know-about-grocery-store-aisles.jpg'
     },
     {
-        type:'nonfood',
+        category:'nonfood',
+        title: 'Non-Food',
         imageURL: 'https://cdnimg.webstaurantstore.com/uploads/seo_category/2020/6/cleaning_toolsv2.jpg'
     }
 
 ]
-const categoriesTitles = ['Produce', 'Meat', 'Pantry/Frozen', 'Non-Food']
-
 // INDEX ROUTE =======================================================
 router.get('/', (req, res) => {
     Item.find({}, (err, allItems) => {
         res.render('index.ejs', {
             items: allItems,
-            categories: categories,
-            titles: categoriesTitles  
+            categories: categories
         })
     })
 })
@@ -38,19 +39,18 @@ router.get('/:category', (req, res) => {
     Item.find({category: req.params.category}, (err, allItems) => {
         res.render(`${req.params.category}.ejs`, {
             items: allItems,
-            category: req.params.category 
+            category: req.params.category, 
+            categories: categories
         })
     })
 })
 // SHOW ROUTE ========================================================
 router.get('/:category/:id', (req, res) => {
     Item.findById(req.params.id, (err, item) => {
-        console.log(item)
-
         res.render('show.ejs', {
             item: item,
             category: req.params.category,
-            id: req.params.id
+            id: req.params.id,
         })
     })
 })
