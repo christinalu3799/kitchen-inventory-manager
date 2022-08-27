c5886483c8c5d28595aa903440c3c6398ee51a08
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-26 23:02:52 -0400

    created users model

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ require('dotenv').config()
 const PORT = process.env.PORT 
 const MONGODB_URI = process.env.MONGODB_URI
 const session = require('express-session')
+const bcrypt = require('bcrypt')
+
+// Hash String with bcrypt
+const hashedString = bcrypt.hashSync('')
 
 // MIDDLEWARE ===================================================
 app.use(express.static("public"));
