c497f51a49176cbd651e37edde5e12818086e390
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-08-26 22:16:05 -0400

    added buttons to table view allowing user to filter list by category

--- public/script.js
+++ public/script.js
@@ -1,1 +1,44 @@
-console.log('script is connected.')
+let rows = document.querySelectorAll('.row')
+console.log(rows)
+
+document.getElementById('all-btn').addEventListener('click', () => {
+    rows.forEach(row => {
+        row.style.display = 'table-row'
+    })
+})
+document.getElementById('produce-btn').addEventListener('click', () => {
+    rows.forEach(row => {
+        if (row.classList.contains('produce')) {
+            row.style.display = 'table-row'
+        } else {
+            row.style.display = 'none'
+        }
+    })
+})
+document.getElementById('meat-btn').addEventListener('click', () => {
+    rows.forEach(row => {
+        if (row.classList.contains('meat')) {
+            row.style.display = 'table-row'
+        } else {
+            row.style.display = 'none'
+        }
+    })
+})
+document.getElementById('pantry-btn').addEventListener('click', () => {
+    rows.forEach(row => {
+        if (row.classList.contains('pantryOrFrozen')) {
+            row.style.display = 'table-row'
+        } else {
+            row.style.display = 'none'
+        }
+    })
+})
+document.getElementById('nonfood-btn').addEventListener('click', () => {
+    rows.forEach(row => {
+        if (row.classList.contains('nonfood')) {
+            row.style.display = 'table-row'
+        } else {
+            row.style.display = 'none'
+        }
+    })
+})
